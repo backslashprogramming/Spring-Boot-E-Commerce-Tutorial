682706efcb2396e5425525c2b0995f2efa40d3bf
Backslash Programming <backslashprogrammingtutorials@gmail.com>
2022-11-12 15:46:59 +0000

    Episode Eight: EndpointSecurity

--- src/main/java/com/youtube/tutorial/ecommercebackend/api/security/WebSecurityConfig.java
+++ src/main/java/com/youtube/tutorial/ecommercebackend/api/security/WebSecurityConfig.java
@@ -4,12 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 /**
  * Configuration of the security on endpoints.
  */
 @Configuration
 public class WebSecurityConfig {
+
+  private JWTRequestFilter jwtRequestFilter;
+
+  public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+    this.jwtRequestFilter = jwtRequestFilter;
+  }
 
   /**
    * Filter chain to configure security.
@@ -19,9 +26,14 @@ public class WebSecurityConfig {
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //TODO: Proper authentication.
     http.csrf().disable().cors().disable();
-    http.authorizeHttpRequests().anyRequest().permitAll();
+    // We need to make sure our authentication filter is run before the http request filter is run.
+    http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+    http.authorizeHttpRequests()
+        // Specific exclusions or rules.
+        .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+        // Everything else should be authenticated.
+        .anyRequest().authenticated();
     return http.build();
   }
 
