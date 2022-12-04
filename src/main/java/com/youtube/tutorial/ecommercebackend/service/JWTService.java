1f29e24e7a257e65962c8c5b8ad492bd8ce72986
Backslash Programming <backslashprogrammingtutorials@gmail.com>
2022-12-04 13:05:38 +0000

    Episode Fifteen: Major Security Flaw!

--- src/main/java/com/youtube/tutorial/ecommercebackend/service/JWTService.java
+++ src/main/java/com/youtube/tutorial/ecommercebackend/service/JWTService.java
@@ -2,6 +2,7 @@ package com.youtube.tutorial.ecommercebackend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.youtube.tutorial.ecommercebackend.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -70,7 +71,8 @@ public class JWTService {
    * @return The username stored inside.
    */
   public String getUsername(String token) {
-    return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+    DecodedJWT jwt = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+    return jwt.getClaim(USERNAME_KEY).asString();
   }
 
 }
--- src/test/java/com/youtube/tutorial/ecommercebackend/service/JWTServiceTest.java
+++ src/test/java/com/youtube/tutorial/ecommercebackend/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.youtube.tutorial.ecommercebackend.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.youtube.tutorial.ecommercebackend.model.LocalUser;
 import com.youtube.tutorial.ecommercebackend.model.dao.LocalUserDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,6 +26,8 @@ public class JWTServiceTest {
    /** The Local User DAO. */
   @Autowired
   private LocalUserDAO localUserDAO;
+  @Value("${jwt.algorithm.key}")
+  private String algorithmKey;
 
   /**
    * Tests that the verification token is not usable for login.
@@ -40,6 +47,32 @@ public class JWTServiceTest {
     LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
     String token = jwtService.generateJWT(user);
     Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for auth should contain users username.");
+  }
+
+  /**
+   * Tests that when someone generates a JWT with an algorithm different to
+   * ours the verification rejects the token as the signature is not verified.
+   */
+  @Test
+  public void testJWTNotGeneratedByUs() {
+    String token =
+        JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+        "NotTheRealSecret"));
+    Assertions.assertThrows(SignatureVerificationException.class,
+        () -> jwtService.getUsername(token));
+  }
+
+  /**
+   * Tests that when a JWT token is generated if it does not contain us as
+   * the issuer we reject it.
+   */
+  @Test
+  public void testJWTCorrectlySignedNoIssuer() {
+    String token =
+        JWT.create().withClaim("USERNAME", "UserA")
+            .sign(Algorithm.HMAC256(algorithmKey));
+    Assertions.assertThrows(MissingClaimException.class,
+        () -> jwtService.getUsername(token));
   }
 
 }
